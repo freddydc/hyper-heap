76e1f860928218e754817477424f1a090c0fe434
freddydc <freddydiaz.liv@gmail.com>
2022-06-20 18:55:48 -0500

    Add query using fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,37 @@
+import { useEffect } from 'react'
 import { Layout } from '../components/common'
 import styles from '../styles/Home.module.css'
 
+const fetchPlantCollection = () =>
+  // Polyfill `fetch` (browser and server)
+  fetch(
+    `https://graphql.contentful.com/content/v1/spaces/${process.env.NEXT_PUBLIC_SPACE_ID}`,
+    {
+      headers: {
+        'Content-type': 'application/json',
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_ACCESS_TOKEN}`
+      },
+      method: 'POST',
+      body: JSON.stringify({
+        query: `query {
+          plantCollection(limit: 5) {
+            limit
+            items {
+              slug
+            }
+          }
+        }`
+      })
+    }
+  )
+
 const Home = () => {
+  useEffect(() => {
+    fetchPlantCollection()
+      .then(response => response.json())
+      .then(result => console.log(result.data))
+  }, [])
+
   return (
     <Layout>
       <h1 className={styles.title}>Welcome Hyper Heap</h1>
